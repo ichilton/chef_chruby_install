8307add4ed5379e1552c23ad81d1908bb3ed59bc
Ian Chilton <ian@ichilton.co.uk>
2013-10-11 13:29:25 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# chruby_install cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chruby_install'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chruby_install'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
