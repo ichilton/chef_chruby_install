aa8c7b73e445b7f5838b56c5fb771dfb319c93b3
Ian Chilton <ian@ichilton.co.uk>
2013-10-11 15:15:00 +0000

    Add supported os into metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,14 @@ license          'All rights reserved'
 description      'Install chruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports "ubuntu"
+supports "debian"
+supports "freebsd"
+supports "redhat"
+supports "centos"
+supports "fedora"
+supports "amazon"
+supports "scientific"
+supports "suse"
+supports "mac_os_x"
